7958ecf6f4872867a0dc40ce11d478cc48badc39
BLAKG07D <blakg07d@gmail.com>
2023-04-20 16:34:39 -0500

    bare minimum setup

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.once("ready", () => {
+    console.log('xLOUD-9 is Ready...')
+})
+client.on("messageCreate", (message) => {
+    console.log(message)
+})
+client.login(process.env.BOT_TOKEN);
